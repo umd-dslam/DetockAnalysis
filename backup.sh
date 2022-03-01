bad083f3d4db1bd1ba870ee76c3a5fa83c3c09cd
Cuong Nguyen <ctring23@gmail.com>
2022-03-01 14:41:35 -0500

    Back up by experiments

--- backup.sh
+++ backup.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
+experiments="cockroach \
+             cockroach-latency \
+             tpcc \
+             ycsb \
+             ycsb-asym \
+             ycsb-jitter \
+             ycsb-jitter-overshoot10 \
+             ycsb-latency"
+experiments="ycsb-jitter-overshoot10"
 
-find main -type f                     \
-  \( -name "*.tar.gz" -o              \
-     -name "*.parquet" -o             \
-     -name "*.pickle" -o              \
-     -name "crdb-latency.csv" -o      \
-     -name "crdb.csv"                 \
-  \)                                  \
-  -not -path "./.ipynb_checkpoints/*" \
-  > backup-files.txt
-
-# tar -czvf detock.tar.gz -T backup-files.txt
+for exp in $experiments
+do
+  find main/$exp -type f -name "*.tar.gz" > $exp-files.txt
+  tar -czvf $exp.tar.gz -T $exp-files.txt
+done
