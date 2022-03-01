e994fed877cd3ab4b3fd8d7b6f9aacf5c79ca988
Cuong Nguyen <ctring23@gmail.com>
2022-03-01 13:36:37 -0500

    Update backup

--- backup.sh
+++ backup.sh
@@ -2,12 +2,10 @@
 
 find main -type f                     \
   \( -name "*.tar.gz" -o              \
-     -name "*.parquet" -o             \
-     -name "*.pickle" -o              \
      -name "crdb-latency.csv" -o      \
      -name "crdb.csv"                 \
   \)                                  \
   -not -path "./.ipynb_checkpoints/*" \
   > backup-files.txt
 
-# tar -czvf detock.tar.gz -T backup-files.txt
+tar -czvf detock.tar.gz -T backup-files.txt
